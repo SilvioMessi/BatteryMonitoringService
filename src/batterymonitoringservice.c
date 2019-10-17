044f97dfab65dc1fb124519737fb9821804fc67a
Silvio Messi <messisilvio@gmail.com>
2019-10-18 01:23:56 +0200

    Made some modification to make the service work properly on a real device. Sampling frequency reduced to to 2 minutes, let's see the battery drain

--- src/batterymonitoringservice.c
+++ src/batterymonitoringservice.c
@@ -6,6 +6,11 @@
 #include <device/battery.h>
 #include <time.h>
 
+struct appdata {
+	Ecore_Timer *timer;
+};
+typedef struct appdata appdata_s;
+
 static Eina_Bool timeout_func(void *data) {
 	time_t raw_time;
 	struct tm* time_info;
@@ -36,13 +41,35 @@ static Eina_Bool timeout_func(void *data) {
 	// log metrics
 	strftime(time_text, sizeof(time_text), "%Y-%m-%dT%H:%M:%S", time_info);
 	insert_sample(time_text, battery_percentage, battery_is_charging_int);
-	return ECORE_CALLBACK_PASS_ON;
+	return ECORE_CALLBACK_RENEW;
 }
 
 bool app_create(void *data) {
 	dlog_print(DLOG_DEBUG, LOG_TAG, "app_create");
+	appdata_s *ad = (appdata_s *) data;
+
+	/* ecore setup
+	 * to make ecore work on a real device I had to add the background category type sensor in the tizen-manifext.xmx
+	 * without it the timeout_func was called only once
+	 */
+	if (!ecore_init()) {
+		dlog_print(DLOG_ERROR, LOG_TAG, "cannot init ecore");
+		// if false is returned the app is terminated
+		return false;
+	}
+	if (ad->timer == NULL) {
+		Ecore_Timer *my_timer = ecore_timer_add(60 * 2, timeout_func, &ad);
+		if (my_timer != NULL) {
+			ad->timer = my_timer;
+			dlog_print(DLOG_DEBUG, LOG_TAG, "ecore timer successfully created");
+		} else {
+			dlog_print(DLOG_ERROR, LOG_TAG, "cannot create ecore timer");
+			return false;
+		}
+	}
+
+	// data control setup
 	initialize_datacontrol_provider();
-	ecore_timer_add(60*15, timeout_func, NULL);
 	return true;
 }
 
@@ -52,11 +79,15 @@ void app_control(app_control_h app_control, void *data) {
 
 void app_terminate(void *data) {
 	dlog_print(DLOG_DEBUG, LOG_TAG, "app_terminate");
+	appdata_s *ad = (appdata_s *) data;
+	if (ad->timer)
+		ecore_timer_del(ad->timer);
 	return;
 }
 
 int main(int argc, char *argv[]) {
 	int ret;
+	appdata_s ad = { 0, };
 
 	service_app_lifecycle_callback_s event_callback = { 0, };
 
@@ -64,7 +95,7 @@ int main(int argc, char *argv[]) {
 	event_callback.terminate = app_terminate;
 	event_callback.app_control = app_control;
 
-	ret = service_app_main(argc, argv, &event_callback, NULL);
+	ret = service_app_main(argc, argv, &event_callback, &ad);
 	if (ret != APP_ERROR_NONE)
 		dlog_print(DLOG_ERROR, LOG_TAG, "main() failed. err = %d", ret);
 
